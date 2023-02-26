def2e8b52402599cdfacdefbad355c684e0952f7
Mohamed Issam Bakha <issam@MacBook-Pro-de-Mohamed.local>
2023-02-26 22:05:02 +0100

    Added some TodoFooter unit tests

---
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+import TodoFooter from "../TodoFooter";
+const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
+  return (
+    <BrowserRouter>
+      <TodoFooter numberOfIncompleteTasks={numberOfIncompleteTasks} />
+    </BrowserRouter>
+  );
+};
+
+test("should render the correct number of incomplet tasks", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={2} />);
+  const pElement = screen.getByText(/2 tasks left/i);
+  expect(pElement).toBeInTheDocument();
+});
+
+it('should render "task" when the number of incomplete tasks is one', () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const pElement = screen.getByText(/1 task left/i);
+  expect(pElement).toBeInTheDocument();
+});
