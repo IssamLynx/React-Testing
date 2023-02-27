f7cf419f7e936c5391423bbc72f8bc8223acfaf3
IssamLynx <hm_bakha@esi.dz>
2023-02-27 16:20:03 +0100

    Added tests for FollowersList component

--- src/components/FollowersList/FollowersList.js
+++ src/components/FollowersList/FollowersList.js
@@ -19,7 +19,11 @@ export default function FollowersList() {
     <div className="followerslist-container">
       <div>
         {followers.map((follower, index) => (
-          <div key={index} className="follower-item">
+          <div
+            data-testid={`follower-item-${index}`}
+            key={index}
+            className="follower-item"
+          >
             <img src={follower.picture.large} />
             <div className="followers-details">
               <div className="follower-item-name">
---
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import FollowersList from "../FollowersList";
+import { BrowserRouter } from "react-router-dom";
+
+const MockFollowersList = () => {
+  return (
+    <BrowserRouter>
+      <FollowersList />
+    </BrowserRouter>
+  );
+};
+describe("FollowerList", () => {
+  test("should fetch and render folowwer element", async () => {
+    render(<MockFollowersList />);
+    const followerItem = await screen.findByTestId(/follower-item-0/i);
+    expect(followerItem).toBeInTheDocument();
+  });
+
+  test("should render multiple followers items", async () => {
+    render(<MockFollowersList />);
+    const followerItems = await screen.findAllByTestId(/follower-item/i);
+    expect(followerItems.length).toBe(5);
+  });
+});
